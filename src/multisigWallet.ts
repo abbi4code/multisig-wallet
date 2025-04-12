882c26c052563df9619b64e16bbc1d413fb56671
abhishek <abhishek4code@gmail.com>
2025-04-12 20:20:33 +0530

    fix: resolve invalid amount error with raw tx funding, avoided fee estimation issues by controlling tx inputs

--- src/multisigWallet.ts
+++ src/multisigWallet.ts
@@ -78,18 +78,40 @@ async function fundWallet(walletData: any): Promise<string> {
     await client.command("generatetoaddress", 101, newAddress);
 
     //2. Checking the wallet balance
-    const balance = await client.command("getbalance");
-    console.log("Wallet balance after mining:", balance, "BTC");
+    const spendableBalance = await client.command("getbalance");
+    const walletInfo = await client.command("getwalletinfo");
+    console.log("Wallet balance details:", {
+      spendable: spendableBalance,
+      immature: walletInfo.immature_balance,
+      total: walletInfo.balance + walletInfo.immature_balance,
+    });
+
+    const utxos = await client.command("listunspent");
+    if(utxos.length === 0){
+      throw new Error("No UTXOs available to spend");
+    }
+
+    const firstUTXO = utxos[0];
+    const amountTosend = 0.01;
+    const change = parseFloat(firstUTXO.amount) - amountTosend - 0.0001;
+
+    // !explain this raw transaction
+    // create raw transaction
+    const rawTx = await client.command("createrawtransaction",[{
+      txid: firstUTXO.txid, vout: firstUTXO.vout
+    }],
+  {[walletData.address]: amountTosend, [newAddress]:change})
+
+    const signedTx = await client.command('signrawtransactionwithwallet',rawTx);
+
+    const txid = await client.command("sendrawtransaction",signedTx.hex);
+
+
+
 
     //3. Send coins to multisig address
-    const amountTosend = 1;
     console.log(
       `Sending ${amountTosend} BTC to multisig address: ${walletData.address}`
-    );
-    const txid = await client.command(
-      "sendtoaddress",
-      walletData.address,
-      amountTosend
     );
 
     //4. So, to confirm this transaction, we need to mine a block
